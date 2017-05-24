b8b93bf7be0e5806502f1cef79016731790e5ad2
Hugo Carneiro <hcarneiro@weboo.biz>
2017-05-24 11:30:00 +0100

    User Register function

--- js/build.js
+++ js/build.js
@@ -1,48 +1,46 @@
 Fliplet().then(function() {
-  var $el = $('[data-about-app-id]:eq(0)');
-  var id = $el.attr('data-about-app-id');
-  var data = Fliplet.Widget.getData(id);
+  Fliplet.Widget.register('About', function() {
 
-  var clientData = {
-    app_name: Fliplet.Env.get('appName'),
-    organisation_name: Fliplet.Env.get('organizationName'),
-    current_year: moment().format('YYYY')
-  }
+    var $el = $('[data-about-app-id]:eq(0)');
+    var id = $el.attr('data-about-app-id');
+    var data = Fliplet.Widget.getData(id);
 
-  var clientInfo = Handlebars.compile(data.infoTemplate);
-  var clientInfoHtml = clientInfo(clientData);
+    var clientData = {
+      app_name: Fliplet.Env.get('appName'),
+      organisation_name: Fliplet.Env.get('organizationName'),
+      current_year: moment().format('YYYY')
+    }
 
-  var aboutOverlay = new Fliplet.Utils.Overlay($('template[name="overlay-about-app"]').html(), {
-    showOnInit: false,
-    closeAnywhere: true,
-    entranceAnim: 'fadeInDown',
-    exitAnim: 'fadeOutUp',
-    size: 'compact',
-    uniqueId: 'about-app'
-  }, function overlayInitialised(el) {
-    $(el.overlay).find('.client-info').html(clientInfoHtml);
-  });
+    var clientInfo = Handlebars.compile(data.infoTemplate);
+    var clientInfoHtml = clientInfo(clientData);
 
-  window.Fliplet = window.Fliplet || {};
-  var Fliplet = window.Fliplet;
+    var aboutOverlay = new Fliplet.Utils.Overlay($('template[name="overlay-about-app"]').html(), {
+      showOnInit: false,
+      closeAnywhere: true,
+      entranceAnim: 'fadeInDown',
+      exitAnim: 'fadeOutUp',
+      size: 'compact',
+      uniqueId: 'about-app'
+    }, function overlayInitialised(el) {
+      $(el.overlay).find('.client-info').html(clientInfoHtml);
+    });
 
-  function open() {
-    aboutOverlay.open();
-  }
+    function open() {
+      aboutOverlay.open();
+    }
 
-  function close() {
-    aboutOverlay.close();
-  }
+    function close() {
+      aboutOverlay.close();
+    }
 
-  function get() {
-    return aboutOverlay;
-  }
+    function get() {
+      return aboutOverlay;
+    }
 
-  Fliplet.About = (function() {
     return {
       get: get,
       open: open,
       close: close
     }
-  })();
+  });
 });
